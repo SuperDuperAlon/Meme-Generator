6f95e6334a9fadf1dcc322b8096c0d6ccef33a79
Alon Mlievski <alonmlievski@gmail.com>
2022-12-14 17:41:58 +0200

    debugging image rendering

--- service/meme-service.js
+++ service/meme-service.js
@@ -15,25 +15,25 @@ var gMeme = {
 };
 
 var gImgs = [
-  { id: 1, url: "/assets/1.jpg", keywords: ["funny", "cat"] },
-  // { id: 2, url: "/assets/2.jpg", keywords: ["funny", "cat"] },
-  // { id: 3, url: "/assets/3.jpg", keywords: ["funny", "cat"] },
-  // { id: 4, url: "/assets/4.jpg", keywords: ["funny", "cat"] },
-  // { id: 5, url: "/assets/5.jpg", keywords: ["funny", "cat"] },
-  // { id: 6, url: "/assets/6.jpg", keywords: ["funny", "cat"] },
-  // { id: 7, url: "/assets/7.jpg", keywords: ["funny", "cat"] },
-  // { id: 8, url: "/assets/8.jpg", keywords: ["funny", "cat"] },
-  // { id: 9, url: "/assets/9.jpg", keywords: ["funny", "cat"] },
-  // { id: 10, url: "/assets/10.jpg", keywords: ["funny", "cat"] },
-  // { id: 11, url: "/assets/11.jpg", keywords: ["funny", "cat"] },
-  // { id: 12, url: "/assets/12.jpg", keywords: ["funny", "cat"] },
-  // { id: 13, url: "/assets/13.jpg", keywords: ["funny", "cat"] },
-  // { id: 14, url: "/assets/14.jpg", keywords: ["funny", "cat"] },
-  // { id: 15, url: "/assets/15.jpg", keywords: ["funny", "cat"] },
-  // { id: 16, url: "/assets/16.jpg", keywords: ["funny", "cat"] },
-  // { id: 17, url: "/assets/17.jpg", keywords: ["funny", "cat"] },
-  // { id: 18, url: "/assets/18.jpg", keywords: ["funny", "cat"] },
-  { id: 19, url: "/assets/19.jpg", keywords: ["funny", "cat"] },
+  { id: 1, url: "./assets/1.jpg", keywords: ["funny", "cat"] },
+  // { id: 2, url: "./assets/2.jpg", keywords: ["funny", "cat"] },
+  // { id: 3, url: "./assets/3.jpg", keywords: ["funny", "cat"] },
+  // { id: 4, url: "./assets/4.jpg", keywords: ["funny", "cat"] },
+  // { id: 5, url: "./assets/5.jpg", keywords: ["funny", "cat"] },
+  // { id: 6, url: "./assets/6.jpg", keywords: ["funny", "cat"] },
+  // { id: 7, url: "./assets/7.jpg", keywords: ["funny", "cat"] },
+  // { id: 8, url: "./assets/8.jpg", keywords: ["funny", "cat"] },
+  // { id: 9, url: "./assets/9.jpg", keywords: ["funny", "cat"] },
+  // { id: 10, url: "./assets/10.jpg", keywords: ["funny", "cat"] },
+  // { id: 11, url: "./assets/11.jpg", keywords: ["funny", "cat"] },
+  // { id: 12, url: "./assets/12.jpg", keywords: ["funny", "cat"] },
+  // { id: 13, url: "./assets/13.jpg", keywords: ["funny", "cat"] },
+  // { id: 14, url: "./assets/14.jpg", keywords: ["funny", "cat"] },
+  // { id: 15, url: "./assets/15.jpg", keywords: ["funny", "cat"] },
+  // { id: 16, url: "./assets/16.jpg", keywords: ["funny", "cat"] },
+  // { id: 17, url: "./assets/17.jpg", keywords: ["funny", "cat"] },
+  // { id: 18, url: "./assets/18.jpg", keywords: ["funny", "cat"] },
+  { id: 19, url: "./assets/19.jpg", keywords: ["funny", "cat"] },
 ];
 
 function getMeme() {
@@ -63,7 +63,7 @@ function setColor(inputValue) {
 }
 
 function setFontSize(val) {
-  gMeme.lines[0].size += val  
+  gMeme.lines[0].size += val;
   console.log(gMeme.lines[0].size);
   _saveMemeToStorage();
   return gMeme;
@@ -71,7 +71,7 @@ function setFontSize(val) {
 
 function setImg(value) {
   gMeme.selectedImgId = value;
-  gMeme.lines[0].txt = 'Add Text Here' // Set defense - if currId is equal to set Id - return
+  gMeme.lines[0].txt = "Add Text Here"; // Set defense - if currId is equal to set Id - return
   _saveMemeToStorage();
 }
 
