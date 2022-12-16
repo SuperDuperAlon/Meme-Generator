0f0b66348dbfcd274399714b40c99d66faf81e52
Alon Mlievski <alonmlievski@gmail.com>
2022-12-16 11:17:13 +0200

    download and share to facebook functionality completed

--- meme-controller.js
+++ meme-controller.js
@@ -26,7 +26,6 @@ function renderText() {
         meme.lines[0].fontSize,
         meme.lines[0].textAlign,
         meme.lines[0].fontStyle
-
       );
     } else if (i === 1) {
       drawText(
@@ -112,7 +111,41 @@ function onChangeAlign(value) {
   renderMeme();
 }
 function onChangeFontStyle(value) {
-  changeFontStyle(value)
+  changeFontStyle(value);
   renderMeme();
 }
 
+function onDownloadImage(elLink) {
+  console.log("dk");
+  const imgContent = gElCanvas.toDataURL("image/jpeg"); // image/jpeg the default format
+  elLink.href = imgContent;
+}
+
+function onShareFb() {
+  const imgDataUrl = gElCanvas.toDataURL("image/jpeg"); // Gets the canvas content as an image format
+
+  // A function to be called if request succeeds
+  function onSuccess(uploadedImgUrl) {
+    // Encode the instance of certain characters in the url
+    const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl);
+    window.open(
+      `https://www.facebook.com/sharer/sharer.php?u=${encodedUploadedImgUrl}&t=${encodedUploadedImgUrl}`
+    );
+  }
+  // Send the image to the server
+  doUploadImg(imgDataUrl, onSuccess);
+}
+
+function doUploadImg(imgDataUrl, onSuccess) {
+  // Pack the image for delivery
+  const formData = new FormData();
+  formData.append("img", imgDataUrl);
+  console.log("formData:", formData);
+  // Send a post req with the image to the server
+  fetch("//ca-upload.com/here/upload.php", { method: "POST", body: formData })
+    .then((res) => res.text())
+    .then((url) => {
+      console.log("url:", url);
+      onSuccess(url);
+    });
+}
